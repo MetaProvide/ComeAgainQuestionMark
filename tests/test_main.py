29eefd246b61ac2538f0bf1f69d46310554d17f6
Henry Bergström <henrybergstrom@protonmail.com>
2021-10-11 00:19:28 +0200

    test: similarity ratio
    difflib looks at the transcription and benchmark to verify it is over 85% similar to pass.

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from zipfile import ZipFile
 from urllib.request import urlretrieve
 import os
 import logging
+import difflib as dl
 
 LOGGER = logging.getLogger(__name__)
 
@@ -20,9 +21,20 @@ def download_assets(url):
     os.remove(zippath)
 
 
-def test_mp3_to_txt_to_timestamp(test_audio, model, benchmark):
+def test_mp4_to_txt_no_timestamp():
+    model = os.path.join(dirname, "tests/assets/models/vosk-model-en-us-0.20")
+    video = os.path.join(dirname, "tests/assets/audio/test_case_1.mp3")
+    benchmark = os.path.join(dirname, "tests/assets/transcription/test_case_1.txt")
+    if (
+        not os.path.exists(model)
+        or not os.path.exists(video)
+        or not os.path.exists(benchmark)
+    ):
+        print("Error with paths")
+        assert False
+
     transcription = transcribe(
-        test_audio,
+        video,
         None,
         model,
         enable_timestamp=False,
@@ -31,19 +43,11 @@ def test_mp3_to_txt_to_timestamp(test_audio, model, benchmark):
     )
 
     with open(benchmark) as f:
-        benchmark_text = f.readlines()
-        if benchmark_text and transcription:
-            assert True
+        benchmark_text = "".join(f.readlines())
+        assert get_similarity_ratio(benchmark_text, transcription) > 0.85
 
 
-def test_mp4_to_raw_no_timestamp():
-    pass
-
-
-if __name__ == "__main__":
-    if not os.path.exists("tests/assets"):
-        download_assets(test_assets_url)
-
+def test_mp3_to_txt_no_timestamp():
     model = os.path.join(dirname, "tests/assets/models/vosk-model-en-us-0.20")
     audio = os.path.join(dirname, "tests/assets/audio/test_case_1.mp3")
     benchmark = os.path.join(dirname, "tests/assets/transcription/test_case_1.txt")
@@ -53,6 +57,45 @@ if __name__ == "__main__":
         or not os.path.exists(benchmark)
     ):
         print("Error with paths")
-        exit(1)
+        assert False
 
-    test_mp3_to_txt_to_timestamp(audio, model, benchmark)
+    transcription = transcribe(
+        audio,
+        None,
+        model,
+        enable_timestamp=False,
+        num_words=7,
+        timestamp_format="txt",
+    )
+
+    with open(benchmark) as f:
+        benchmark_text = "".join(f.readlines())
+        assert get_similarity_ratio(benchmark_text, transcription) > 0.85
+
+
+def get_similarity_ratio(benchmark_text, transcription_text):
+    compare1 = [word for word in benchmark_text.lower().split(" ") if word]
+    compare2 = [
+        word
+        for word in transcription_text.lower().replace("\n", " ").split(" ")
+        if word
+    ]
+
+    sequence_matcher = dl.SequenceMatcher()
+    sequence_matcher.set_seqs(compare1, compare2)
+
+    simularity_ratio = sequence_matcher.ratio()
+    print("Similarity Ratio: ", simularity_ratio)
+
+    for diff in dl.context_diff(compare1, compare2):
+        print(diff)
+
+    return simularity_ratio
+
+
+if __name__ == "__main__":
+    if not os.path.exists("tests/assets"):
+        download_assets(test_assets_url)
+
+    test_mp4_to_txt_no_timestamp()
+    test_mp3_to_txt_no_timestamp()
