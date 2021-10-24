75233bdd1e7a313945d7b7a0c9c63a7de1344c2a
Henry Bergström <henrybergstrom@protonmail.com>
2021-10-24 18:33:47 +0200

    test: works on local

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,27 +10,49 @@ LOGGER = logging.getLogger(__name__)
 
 test_assets_url = "https://space.metaprovide.org/index.php/s/qgsSMZk2WqcZ8FR/download"
 dirname = os.getcwd()
+test_folder_path = os.path.join(dirname, "tests")
+test_assets_folder_path = os.path.join(test_folder_path, "assets")
+
+
+def setup_module(module):
+    if not os.path.exists(test_assets_folder_path):
+        print("----- Setup Assets ------")
+        download_assets(test_assets_url)
+        print(os.system(f"tree {test_assets_folder_path}"))
+
+
+def teardown_module(module):
+    print("------ Teardown Done -------")
 
 
 def download_assets(url):
-    zippath = os.path.join(dirname, "assets.zip")
-    urlretrieve(test_assets_url, zippath)
-    zipfile = ZipFile(zippath, "r")
-    zipfile.extractall()
+    zip_path = os.path.join(f"{test_assets_folder_path}.zip")
+    print(f"Zipping to {zip_path}")
+    urlretrieve(url, zip_path)
+    zipfile = ZipFile(zip_path, "r")
+    zipfile.extractall(test_folder_path)
+    print(f"Extracting zip file to {test_assets_folder_path}")
     zipfile.close()
-    os.remove(zippath)
+    os.remove(zip_path)
+    print(f"Removing zip file {zip_path}")
 
 
 def test_mp4_to_txt_no_timestamp():
     model = os.path.join(dirname, "tests/assets/models/vosk-model-en-us-0.20")
-    video = os.path.join(dirname, "tests/assets/audio/test_case_1.mp3")
-    benchmark = os.path.join(dirname, "tests/assets/transcription/test_case_1.txt")
-    if (
-        not os.path.exists(model)
-        or not os.path.exists(video)
-        or not os.path.exists(benchmark)
-    ):
-        print("Error with paths")
+    video = os.path.join(dirname, "tests/assets/video/test_case_1.mp4")
+    benchmark_transcription = os.path.join(
+        dirname, "tests/assets/transcription/test_case_1.txt"
+    )
+    if not os.path.exists(model):
+        print(f"Error with path {model}")
+        assert False
+
+    if not os.path.exists(video):
+        print(f"Error with path {video}")
+        assert False
+
+    if not os.path.exists(benchmark_transcription):
+        print(f"Error with path {benchmark_transcription}")
         assert False
 
     transcription = transcribe(
@@ -42,7 +64,7 @@ def test_mp4_to_txt_no_timestamp():
         timestamp_format="txt",
     )
 
-    with open(benchmark) as f:
+    with open(benchmark_transcription) as f:
         benchmark_text = "".join(f.readlines())
         assert get_similarity_ratio(benchmark_text, transcription) > 0.85
 
@@ -51,12 +73,16 @@ def test_mp3_to_txt_no_timestamp():
     model = os.path.join(dirname, "tests/assets/models/vosk-model-en-us-0.20")
     audio = os.path.join(dirname, "tests/assets/audio/test_case_1.mp3")
     benchmark = os.path.join(dirname, "tests/assets/transcription/test_case_1.txt")
-    if (
-        not os.path.exists(model)
-        or not os.path.exists(audio)
-        or not os.path.exists(benchmark)
-    ):
-        print("Error with paths")
+    if not os.path.exists(model):
+        print(f"Error with path {model}")
+        assert False
+
+    if not os.path.exists(audio):
+        print(f"Error with path {audio}")
+        assert False
+
+    if not os.path.exists(benchmark):
+        print(f"Error with path {benchmark}")
         assert False
 
     transcription = transcribe(
